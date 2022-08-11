0ad42004dfb00c7436e000b7d63f1222b031d989
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-08-12 00:22:29 +0300

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div className="text-red-500">Home Page</div>;
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="font-bold text-2xl text-green-700">HOME</h1>
+    </div>
+  );
 };
 
 export default Home;
