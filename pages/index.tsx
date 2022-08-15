dde7de1f503cc0da0a90ecd11490ad4593040998
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-08-15 23:37:25 +0300

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <h1 className="font-bold text-2xl text-green-700">HOME</h1>
+      <h1 className="font-bold text-2xl text-green-600">HOME</h1>
     </div>
   );
 };
