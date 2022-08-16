dc9ea17644422872f9b505d6325ec3e67fbc49c2
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-08-16 20:28:57 +0300

    file structure

--- src/pages/api/signin.ts
+++ src/pages/api/signin.ts
@@ -1,7 +1,7 @@
 import brcypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookie from "cookie";
-import prisma from "lib/prisma";
+import prisma from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
