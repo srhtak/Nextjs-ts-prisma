5cfd0408c856c93f0023bfcb034ce612f8131ca8
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-08-20 15:33:46 +0300

    remove trpc

--- src/pages/api/trpc/[trpc].ts
+++
@@ -1,8 +0,0 @@
-import * as trpcNext from "@trpc/server/adapters/next";
-import { appRouter } from "@/backend/router/app.routes";
-import { createContext } from "@/backend/router/createContext";
-
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext,
-});
--- src/utils/trpc.ts
+++
@@ -1,5 +0,0 @@
-// src/utils/trpc.ts
-import { AppRouter } from "@/backend/router/app.routes";
-import { createReactQueryHooks } from "@trpc/react";
-
-export const trpc = createReactQueryHooks<AppRouter>();
